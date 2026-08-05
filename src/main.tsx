3e6644bf8b8cae1d7073dc7ca1ae5c1b10ae3444
Falko Schumann <falko.schumann@muspellheim.de>
2026-08-05 21:49:39 +0200

    refactor: rename entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./ui/app.tsx";
+import App from "./ui";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
